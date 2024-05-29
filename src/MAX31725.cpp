cb5bbc0c1f33a3c71bbe86cb7dea1f858f2d9468
Oscar <61410630+ozzwozz@users.noreply.github.com>
2024-05-29 16:33:06 +0100

    Add files via upload

--- src/MAX31725.cpp
+++ src/MAX31725.cpp
@@ -7,6 +7,8 @@ MAX31725::MAX31725(i2c_inst_t *i2c, uint8_t address) : I2CDevice(i2c, address)
     gpio_set_dir(m_overtemp_pin, GPIO_IN);
     gpio_pull_down(m_overtemp_pin); // Set External interrupt trigger as input with pull-down resistor
 
+    set_over_temp_limit();
+    
     // Set up interrupt for GPIO 15
     irq_set_exclusive_handler(15, (irq_handler_t)over_temp_irq_handler);
 
@@ -14,6 +16,18 @@ MAX31725::MAX31725(i2c_inst_t *i2c, uint8_t address) : I2CDevice(i2c, address)
 
 MAX31725::~MAX31725()
 {
+}
+
+bool MAX31725::set_over_temp_limit()
+{
+    uint8_t tx_data[2] {cmd_set_temp_limit, m_temperature_limit_C};
+
+    if (!write(tx_data, 1))
+    {
+        return false;
+    }
+
+    return true;
 }
 
 bool MAX31725::read_temperature(float &temperature)
